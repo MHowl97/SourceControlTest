7f80e25106c21d3539356048b27a1d33b6ec5758
Mike Howlett <mah9882@rit.edu>
2018-06-25 10:44:54 -0400

    Initial commit

---
+++ Source/SourceControlTest/SourceControlTestGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SourceControlTestGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASourceControlTestGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASourceControlTestGameMode();
+};
+
+
+
